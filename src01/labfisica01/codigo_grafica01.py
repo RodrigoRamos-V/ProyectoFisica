05100cb0fcf684fb641c72668712e2db72f5d652
RodrigoRamos-V <202500069@est.umss.edu>
2025-11-16 12:48:37 -0400

    Update codigo_grafica01.py

--- src01/labfisica01/codigo_grafica01.py
+++ src01/labfisica01/codigo_grafica01.py
@@ -2,25 +2,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-def main():
-    data = {
-        "H_cm": [1.00, 2.00, 3.00, 4.00, 5.00, 6.00],
-        "m_g":  [8.65, 17.30, 25.95, 34.63, 43.31, 51.95]
-    }
+data = {
+    "H": [1.00, 2.00, 3.00, 4.00, 5.00, 6.00],
+    "m": [8.65, 17.30, 25.95, 34.63, 43.31, 51.95]
+}
 
-    df = pd.DataFrame(data)
+df = pd.DataFrame(data)
 
-    Path("plots_C1").mkdir(exist_ok=True)
+Path("plots_cil").mkdir(parents=True, exist_ok=True)
 
-    plt.figure(figsize=(8,5))
-    plt.scatter(df["H_cm"], df["m_g"], label="Datos", s=60)
-    plt.plot(df["H_cm"], df["m_g"])
-    plt.title("Tabla C.1 – Cilindros")
-    plt.xlabel("H [cm]")
-    plt.ylabel("m [g]")
-    plt.grid(True)
-    plt.tight_layout()
-
-    plt.savefig("plots_C1/cilindros.png", dpi=150)
-    plt.show()
-    plt.close()
+plt.figure(figsize=(8,5))
+plt.plot(df["H"], df["m"], "o-b", label="Masa")
+plt.title("Masa vs Altura – Cilindros")
+plt.xlabel("Altura H [cm]")
+plt.ylabel("Masa [g]")
+plt.grid(True)
+plt.legend()
+plt.tight_layout()
+plt.savefig("plots_cil/cilindros_masa_vs_altura.png", dpi=150)
+plt.show()
+plt.close()
