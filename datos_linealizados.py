4809842ae507173d9d9cd6fb0f59bc61b8f3c1cf
saidnilsarnez-code <saidnilsarnez@gmail.com>
2025-11-19 17:07:00 -0400

    Modify time and Y values in datos_linealizados.py
    Updated time and Y values for calculations.

--- datos_linealizados.py
+++ datos_linealizados.py
@@ -1,16 +1,2 @@
-import math
-from ProyectoFisica.MMC import MMC
-
-t = [0.000, 0.200, 0.400, 0.600, 0.800, 1.000]
-x = [1.000, 0.951, 0.809, 0.588, 0.309, 0.000]
-
-xmax = max(x)
-Y = [math.acos(xi / xmax) for xi in x]
-
-mmc = MMC(x=t, y=Y)
-
-print("A =", mmc.A)
-print("B =", mmc.B)
-print("sigma² =", mmc.sigma2)
-print("A_err =", mmc.A_err)
-print("B_err =", mmc.B_err)
+t = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+Y = [0.000000, 0.313, 0.627, 0.942, 1.256, 1.571]
