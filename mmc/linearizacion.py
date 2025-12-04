f0682f207f187ec625520fb34cc66cc01331fbcd
RodrigoRamos-V <202500069@est.umss.edu>
2025-12-04 07:12:44 -0400

    Actualizar linearizacion.py

--- mmc/linearizacion.py
+++ mmc/linearizacion.py
@@ -8,14 +8,19 @@ os.makedirs("image", exist_ok=True)
 t = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
 Y = [0.000000, 0.313, 0.627, 0.942, 1.256, 1.571]
 
-mmc = MMC(x=t, y=Y)
+mmc = MMC(x_data=t, y_data=Y)
+
+A, B = mmc.calculate_coefficients()
+dA, dB = mmc.calculate_errors()
 
 x_plot = np.linspace(min(t), max(t), 200)
-y_plot = mmc.A + mmc.B * x_plot
+y_plot = A + B * x_plot
 
 plt.figure(figsize=(7,5))
 plt.scatter(t, Y, label="Datos linearizados")
-plt.plot(x_plot, y_plot, label=f"Ajuste MMC\nA={mmc.A:.4f}±{mmc.A_err:.4f}\nB={mmc.B:.4f}±{mmc.B_err:.4f}")
+plt.plot(x_plot, y_plot,
+         label=f"Ajuste MMC\nA={A:.4f}±{dA:.4f}\nB={B:.4f}±{dB:.4f}")
+
 plt.xlabel("t")
 plt.ylabel("Y = acos(x/xmax)")
 plt.title("Ajuste por Mínimos Cuadrados (MMC)")
@@ -25,4 +30,3 @@ plt.tight_layout()
 
 plt.savefig("image/linearizado_mmc.png", dpi=150)
 plt.close()
-
