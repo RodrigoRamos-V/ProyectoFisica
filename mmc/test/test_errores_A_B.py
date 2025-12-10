a60cf31919efff62b27c1b691d6a14a67fc232fc
RodrigoRamos-V <202500069@est.umss.edu>
2025-12-10 13:43:27 -0400

    Actualizar test_errores_A_B.py

--- mmc/test/test_errores_A_B.py
+++ mmc/test/test_errores_A_B.py
@@ -2,17 +2,16 @@ import math
 from MMC import MMC
 
 def test_errores_A_B():
-    t = [0.000, 0.200, 0.400, 0.600, 0.800, 1.000]
-    x = [1.000, 0.951, 0.809, 0.588, 0.309, 0.000]
+    t = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+    x = [1.0, 0.951, 0.809, 0.588, 0.309, 0.0]
 
-    xmax = max(x)
-    Y = [math.acos(xi / xmax) for xi in x]
+    Y = [math.acos(xi / max(x)) for xi in x]
 
     mmc = MMC(t, Y)
     mmc.calculate_errors()
 
-    A_err_esperado = 5.604509984062e-05
-    B_err_esperado = 5.604509984062e-05
+    A_err_esperado = 5.604320e-05
+    B_err_esperado = 0.00012906577257161536
 
     assert abs(mmc.dA - A_err_esperado) < 1e-6
     assert abs(mmc.dB - B_err_esperado) < 1e-6
