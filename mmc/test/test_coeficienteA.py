fe0e179cde3a2a20d5b47a23cfc2c90e74593dcd
RodrigoRamos-V <202500069@est.umss.edu>
2025-12-10 13:42:34 -0400

    Actualizar test_coeficienteA.py

--- mmc/test/test_coeficienteA.py
+++ mmc/test/test_coeficienteA.py
@@ -2,15 +2,13 @@ import math
 from MMC import MMC
 
 def test_coeficiente_A():
-    t = [0.000, 0.200, 0.400, 0.600, 0.800, 1.000]
-    x = [1.000, 0.951, 0.809, 0.588, 0.309, 0.000]
+    t = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+    x = [1.0, 0.951, 0.809, 0.588, 0.309, 0.0]
 
-    xmax = max(x)
-    Y = [math.acos(xi / xmax) for xi in x]
+    Y = [math.acos(xi / max(x)) for xi in x]
 
     mmc = MMC(t, Y)
     mmc.calculate_coefficients()
 
-    A_esperado = 1.5706331676940446
-
+    A_esperado = 1.5705827644880892
     assert abs(mmc.A - A_esperado) < 1e-6
